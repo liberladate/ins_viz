d3f0d7b5c909e33802ccca143e5f49169c8cb024
Mircea Moise <mmoise@thoughtworks.com>
2014-10-05 21:38:18 +0100

    Using tar rather than gnutar so package works on linux

--- tasks/packaging.rb
+++ tasks/packaging.rb
@@ -1,4 +1,4 @@
 task :package do
   `mkdir -p build`
-  `gnutar -cf build/ins_data_viz.tar.gz app/* public/* views/* data/* config.ru app.rb Gemfile Gemfile.lock Rakefile`
+  `tar -cf build/ins_data_viz.tar.gz app/* public/* views/* data/* config.ru app.rb Gemfile Gemfile.lock Rakefile`
 end
