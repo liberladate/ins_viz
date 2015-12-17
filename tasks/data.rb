c27c1c87a4e6c2e3faa987b760c38e14b658f38f
Mircea Moise <mmoise@thoughtworks.com>
2015-12-17 20:54:38 +0000

    Adding the category and subcategory to the data_set description

--- tasks/data.rb
+++ tasks/data.rb
@@ -3,12 +3,18 @@ task :flatten_categories do
    contents = File.read('public/js/ins_short.js')
    contents = contents.split('var category_data =').last.chop
    categories = JSON.parse(contents)
-   data_sets = categories.values.flat_map {|item| item.values}.map {|hash|
-      arr = []
-      hash.each_pair { |k,v|
-        arr << {name: k, description: v["description"]}
-      }
-      arr
-   }.flatten
+   data_sets = []
+   categories.each_pair do |category, subcategories|
+     subcategories.each_pair do |subcategory, tables|
+       tables.each_pair do |name, description|
+         data_sets << {
+           name: name,
+           description: description['description'],
+           category: category,
+           subcategory: subcategory
+         }
+       end
+     end
+   end
    File.open('data/categories.json', 'w') {|f| f.write(JSON.generate(data_sets))}
 end
