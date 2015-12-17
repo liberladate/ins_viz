638ccdcf3c4c305d4d20f853497f7419f227bbc2
Mircea Moise <mmoise@thoughtworks.com>
2015-12-17 12:51:59 +0000

    Adding basic search page

--- app.rb
+++ app.rb
@@ -4,11 +4,31 @@ require 'faraday'
 require_relative 'app/wiring'
 
 module Ronin
+  class DataSet
+      attr_reader :description
+      def initialize(name: 'none', description: 'none')
+        @name = name
+        @description = description
+      end
+
+      def url
+        "/graph/#{@name}"
+      end
+  end
+
   class Website < Sinatra::Base
     include Wiring
 
     get '/' do
       haml :home
+    end
+
+    get '/search' do
+      top_data_sets = [
+        DataSet.new(name: 'POP101A', description: 'Populatia stabila pe sexe, etc'),
+        DataSet.new(name: 'CON110A', description: 'Productivitatea muncii, etc')
+      ]
+      haml :search, locals: {top_data_sets: top_data_sets}
     end
 
     get '/table/:table_id' do
