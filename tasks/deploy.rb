b0657dc93a5aac5fe80a152b837cf618b72c5a2b
Mircea Moise <mmoise@thoughtworks.com>
2015-12-18 14:38:41 +0000

    Showing the output of ansible run to deploy

--- tasks/deploy.rb
+++ tasks/deploy.rb
@@ -1,5 +1,6 @@
 task :deploy => [:package] do
   puts 'Starting deploying'
   `ansible-playbook infra/main.yml -i infra/hosts -v > /tmp/ansible-output`
+  `cat /tmp/ansible-output`
   puts 'Finished deploying'
 end
