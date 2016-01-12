bdb0e5a854c9d79fb470d44f8fa2d2021d3fd518
Mircea Moise <mmoise@thoughtworks.com>
2016-01-12 22:21:10 +0000

    Added y axis title

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,27 @@
+Chart.types.Line.extend({
+    name: "LineYLegend",
+    draw: function () {
+        Chart.types.Line.prototype.draw.apply(this, arguments);
+
+        var ctx = this.chart.ctx;
+        ctx.save();
+        // text alignment and color
+        ctx.textAlign = "center";
+        ctx.textBaseline = "bottom";
+        ctx.fillStyle = this.options.scaleFontColor;
+        ctx.font = "20px Arial"
+        // position
+        var x = this.scale.xScalePaddingLeft * 0.4;
+        var y = this.chart.height / 2;
+        // change origin
+        ctx.translate(x, y)
+        // rotate text
+        ctx.rotate(-90 * Math.PI / 180);
+        ctx.fillText(this.datasets[0].label, 0, 0);
+        ctx.restore();
+    }
+});
+
 function graph_for_data(file_name, measure_unit) {
   var ctx = $("#graph_area").get(0).getContext("2d");
 
@@ -12,7 +36,7 @@ function graph_for_data(file_name, measure_unit) {
       labels: data.map(function(item) {return item.ani}),
       datasets: [
           {
-              label: "My First dataset",
+              label: measure_unit,
               fillColor: "rgba(220,220,220,0.3)",
               strokeColor: "rgba(220,220,220,1)",
               pointColor: "rgba(220,220,220,1)",
@@ -23,11 +47,10 @@ function graph_for_data(file_name, measure_unit) {
           }
         ]
       };
-    var myNewChart = new Chart(ctx).Line(graphData, {});
+    var myNewChart = new Chart(ctx).LineYLegend(graphData, { scaleLabel: "         <%=value%>"});
   }
 
   d3.csv(file_name, type, function (error, values) {
-    console.log(values);
     draw(values);
   })
 
