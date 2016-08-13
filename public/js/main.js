2ed102e5cc6a253ab50306d023fbc6ae58e99fc9
Mircea M <mircea.moise@gmail.com>
2016-08-13 16:30:55 +0100

    Upgrading to ChartJS 2.2 as it has more features

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,3 @@
-Chart.types.Line.extend({
-    name: "LineYLegend",
-    draw: function () {
-        Chart.types.Line.prototype.draw.apply(this, arguments);
-
-        var ctx = this.chart.ctx;
-        ctx.save();
-        // text alignment and color
-        ctx.textAlign = "center";
-        ctx.textBaseline = "bottom";
-        ctx.fillStyle = this.options.scaleFontColor;
-        ctx.font = "20px Arial"
-        // position
-        var x = this.scale.xScalePaddingLeft * 0.4;
-        var y = this.chart.height / 2;
-        // change origin
-        ctx.translate(x, y)
-        // rotate text
-        ctx.rotate(-90 * Math.PI / 180);
-        ctx.fillText(this.datasets[0].label, 0, 0);
-        ctx.restore();
-    }
-});
-
 function graph_for_data(file_name, measure_unit) {
   var ctx = $("#graph_area").get(0).getContext("2d");
 
@@ -47,7 +23,21 @@ function graph_for_data(file_name, measure_unit) {
           }
         ]
       };
-    var myNewChart = new Chart(ctx).LineYLegend(graphData, { scaleLabel: "           <%=value%>"});
+
+      new Chart(ctx, {
+          type: 'line',
+          data: graphData,
+          options: {
+              scales: {
+                      yAxes: [{
+                          ticks: {
+                              min: 0,
+                              beginAtZero: true
+                          }
+                      }]
+                  }
+          }
+      })
   }
 
   d3.csv(file_name, type, function (error, values) {
