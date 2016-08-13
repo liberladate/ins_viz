d54d3e3d7dc40410ed2fc49a313f24398647654d
Mircea M <mircea.moise@gmail.com>
2016-08-13 14:02:02 +0100

    Updated the name of the table in test to reflect the update in the table names

--- spec/pages_spec.rb
+++ spec/pages_spec.rb
@@ -11,13 +11,13 @@ module Ronin
 
     describe 'Table CSV' do
       it 'should return a 200 page when asking for a table in csv format' do
-        expect(table_csv('POP101A').response_headers['Content-Type']).to include 'text/csv'
+        expect(table_csv('POP105A').response_headers['Content-Type']).to include 'text/csv'
       end
     end
 
     describe 'Table graph page' do
       it 'should return a page with the correct title' do
-        expect(graph('POP101A').title).to include 'Vizualizare grafica a datelor INS'
+        expect(graph('POP105A').title).to include 'Vizualizare grafica a datelor INS'
       end
     end
 
